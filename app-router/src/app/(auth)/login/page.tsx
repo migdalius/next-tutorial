aebdc0564e9e436af9f6903199f3e8274e0e3db6
migdalius <nyjepl@gmail.com>
2023-05-24 10:03:28 +0200

    Loading server side component

--- app-router/src/app/(auth)/login/page.tsx
+++ app-router/src/app/(auth)/login/page.tsx
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const page = async () => {
+  await wait(5000);
+
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/posts/1"
   );
