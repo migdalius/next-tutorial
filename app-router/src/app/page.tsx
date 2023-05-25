3765e4e68138b406d8aa0032a92e667844743bc4
migdalius <nyjepl@gmail.com>
2023-05-25 09:20:59 +0200

    post

--- app-router/src/app/page.tsx
+++ app-router/src/app/page.tsx
@@ -1,6 +1,3 @@
-const session = null;
-
 export default function Home() {
-  if (!session) throw new Error("Auth is required to accesss this resources");
   return <main>this is an auth-only page</main>;
 }
